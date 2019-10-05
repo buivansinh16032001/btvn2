8d7178faca3f0ee0e2af188ae891067d79f19f60
buivansinh <buivansinh16032001@gmail.com>
2019-10-05 18:24:43 +0700

    bai tap 2

---
+++ max minabc.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+       int a,b,c;
+	   printf("Nhap so thu nhat:\n");
+	   scanf("%d",&a);
+	   printf("Nhap so thu 2:\n");
+	   scanf("%d",&b);
+	   printf("Nhap so thu 3:\n");
+	   scanf("%d",&c);
+	   if(a>b){
+	         printf("max: %d   -  min:  %d\n",a,b);	
+}
+	return 0;
+	
+}
+
+
