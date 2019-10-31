7f6aabc5ee8c8a85ae88f0abd5d4f7fc99f94984
buivansinh <buivansinh16032001@gmail.com>
2019-10-31 19:45:41 +0700

    baithi1

---
+++ baithi1.cpp
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main(){
+	int x1=0,x2=1,x3;
+	printf("Day so Fibonacci tu khoang 1 den 100: \n");
+	printf("%d\t",x3);
+	for(int i=2;x2+x3<100;i++)
+	{
+		x1=x2;
+		x2=x3;
+		x3=x1+x2;
+	printf("%d\t",x3);
+	
+	}
+		
+	
+	
+	return 0;
+}
+
+
